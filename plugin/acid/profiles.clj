d954dbff7fa4208cc69d859f0c61bed14b2cb401
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2019-05-11 22:26:32 -0300

    Update profiles.clj

--- plugin/acid/profiles.clj
+++ plugin/acid/profiles.clj
@@ -1,5 +1,21 @@
-{:user {:plugins    [[cider/cider-nrepl "0.21.1"]
-                     [nrepl "0.6.0"]
-                     [refactor-nrepl "2.4.1-SNAPSHOT" :exclusions [nrepl org.clojure/tools.logging]]
-                     [mvxcvi/whidbey "2.1.0"]]
-        }}
+{:user
+ {:plugins
+  []
+
+  :dependencies
+  [[clj-stacktrace "0.2.8"]
+   [pjstadig/humane-test-output "0.9.0" :exclusions  [[org.clojure/clojure]]]
+   [org.clojure/tools.namespace "0.2.11"]]
+  }
+
+ :repl
+ {:plugins
+  [[refactor-nrepl "2.4.1-SNAPSHOT"]
+   [cider/cider-nrepl "0.22.0-SNAPSHOT"]]
+
+  :dependencies
+  [[org.clojure/tools.nrepl "0.2.13" :exclusions [org.clojure/clojure]]]
+
+  :injections
+  [(require '[clojure.string :as string])
+   (require '[clojure.pprint :refer [pprint]])] }}
