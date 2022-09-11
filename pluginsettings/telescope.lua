4ba891ec16a7bd6ccd4dc149aa3b92020635c504
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2022-09-10 23:25:35 -0300

    Telescope fuzzy grep

--- pluginsettings/telescope.lua
+++ pluginsettings/telescope.lua
@@ -1,5 +1,6 @@
 require('telescope').load_extension('fzf')
 
 nnoremap('<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>")
-nnoremap('<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+nnoremap('<leader>fg', ":Telescope grep_string search=")
+nnoremap('<leader>fG', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 nnoremap('<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>")
