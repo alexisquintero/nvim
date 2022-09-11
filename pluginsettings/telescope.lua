d156f2199bb9185c94132693d44d2d2d3e799954
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2022-09-10 23:37:58 -0300

    c-m mapping and resume

--- pluginsettings/telescope.lua
+++ pluginsettings/telescope.lua
@@ -1,6 +1,19 @@
+local actions = require("telescope.actions")
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-m>"] = actions.select_default
+      },
+    },
+  }
+}
+
 require('telescope').load_extension('fzf')
 
-nnoremap('<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>")
-nnoremap('<leader>fg', ":Telescope grep_string search=")
-nnoremap('<leader>fG', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-nnoremap('<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>")
+
+nnoremap('<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>")
+nnoremap('<leader>fg', ":Telescope grep_string search=<CR>")
+nnoremap('<leader>fG', "<cmd>lua require('telescope.builtin').live_grep()<CR>")
+nnoremap('<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<CR>")
+nnoremap('<leader>fr', ":Telescope resume<CR>")
