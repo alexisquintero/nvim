b07399bd1f7585d8b7ec3aa2c4ca162cc562c4a5
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2023-04-14 22:41:43 +0900

    move to lua try 2

---
+++ lua/pluginsettings/init.lua
@@ -0,0 +1,19 @@
+require('pluginsettings/lsp-config') -- Has lsp mappings fn
+
+require('pluginsettings/cmp')
+require('pluginsettings/comment-nvim')
+require('pluginsettings/fidget')
+require('pluginsettings/gitsigns')
+require('pluginsettings/haskell-tools')
+require('pluginsettings/luasnip')
+require('pluginsettings/metals')
+require('pluginsettings/netrw')
+require('pluginsettings/rust-tools')
+require('pluginsettings/sandwich')
+require('pluginsettings/sexp')
+require('pluginsettings/targets')
+require('pluginsettings/telescope')
+require('pluginsettings/treesitter')
+require('pluginsettings/vim-cool')
+require('pluginsettings/vim-printer')
+
