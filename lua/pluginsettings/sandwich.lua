a377b7dcb515c2803c93ca0d819aca368ca471a0
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2024-05-25 20:32:57 +0900

    typos

--- lua/pluginsettings/sandwich.lua
+++ lua/pluginsettings/sandwich.lua
@@ -1,7 +1,7 @@
 return {
   'machakann/vim-sandwich',
   lazy = false,
-  cofig = function()
+  config = function()
     vim.g.textobj_sandwich_no_default_key_mappings = 1
   end
 }
--- lua/pluginsettings/treesitter.lua
+++ lua/pluginsettings/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  requires = {
+  dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects'
   },
   build = ":TSUpdate",
