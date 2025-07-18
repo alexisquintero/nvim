58264cf1edb7d2e539662cef2afeeee509ea46ea
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2025-07-18 14:03:36 +0900

    Add dadbod

---
+++ lua/pluginsettings/vim-dadbod.lua
@@ -0,0 +1,24 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    {
+      'kristijanhusak/vim-dadbod-completion',
+      ft = { 'sql', 'mysql', 'plsql', 'db', 'duckdb', 'ddb' },
+      lazy = true,
+      init = function()
+        vim.opt_local.omnifunc = 'vim_dadbod_completion#omni'
+      end
+    },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
