48e6f8ec8f5ca740d23944629bc28884149d948e
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2025-11-08 15:09:04 +0900

    Update plugins

--- lua/pluginsettings/lsp-config.lua
+++ lua/pluginsettings/lsp-config.lua
@@ -2,12 +2,13 @@ return {
   'neovim/nvim-lspconfig',
   config = function()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
+    vim.lsp.config('*', {
+      capabilities = capabilities,
+    })
 
     local servers = { "clojure_lsp", "nixd", "terraformls", "pyright", "bashls", "cmake", "dockerls", "ts_ls", "lua_ls" }
     for _, lsp in ipairs(servers) do
-      require('lspconfig')[lsp].setup {
-        capabilities = capabilities,
-      }
+      vim.lsp.enable(lsp)
     end
   end
 }
