4e5a17c8f1e187e84aee5a2fa874e58675f654d2
Alexis Quintero <alexis.quintero@paidy.com>
2026-07-27 15:50:36 +0900

    don't auto-trigger firenvim

--- lua/pluginsettings/firenvim.lua
+++ lua/pluginsettings/firenvim.lua
@@ -4,6 +4,15 @@ return {
   -- Lazy load firenvim
   -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
   lazy = not vim.g.started_by_firenvim,
-  build = ":call firenvim#install(0)"
+  build = ":call firenvim#install(0)",
+
+  -- Never take over elements automatically. Trigger manually with <C-e>.
+  init = function()
+    vim.g.firenvim_config = {
+      localSettings = {
+        [".*"] = { takeover = "never", priority = 0 },
+      },
+    }
+  end,
 }
 
