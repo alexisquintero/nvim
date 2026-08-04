ea926eae25ce198607bd750ecfc753fdf2abaceb
Alexis Quintero <alexis.quintero@paidy.com>
2026-08-04 10:40:46 +0900

    fix markview

--- lua/pluginsettings/markview.lua
+++ lua/pluginsettings/markview.lua
@@ -1,15 +1,16 @@
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
+  "OXY2DEV/markview.nvim",
+  lazy = false,
 
-   -- For `nvim-treesitter` users.
-    priority = 49,
-    config = function()
-        require("markview").setup({
-            preview = {
-                ignore_buftypes = {},
-            },
-        })
-        table.insert(require("markview.spec").config.preview.filetypes, "opencode_output")
-    end,
+  -- For `nvim-treesitter` users.
+  priority = 49,
+  config = function()
+    require("markview").setup({
+      preview = {
+        condition = function(buffer)
+          return ({ opencode_output = true })[vim.bo[buffer].filetype]
+        end,
+      },
+    })
+  end,
 };
