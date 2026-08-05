f9de0089ffd277c900ade45abf937103de53bf04
Alexis Quintero <alexis.quintero@paidy.com>
2026-08-05 17:00:05 +0900

    limit skk

--- lua/pluginsettings/skkeleton.lua
+++ lua/pluginsettings/skkeleton.lua
@@ -1,6 +1,7 @@
 return {
   'vim-skk/skkeleton',
   dependencies = { 'vim-denops/denops.vim' },
+  ft = 'aozora',
   config = function()
     vim.keymap.set({ 'i', 'c' }, '<C-j>', '<Plug>(skkeleton-toggle)')
     vim.fn['skkeleton#config']({
