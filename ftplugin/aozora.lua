54b6655776c25be0a3e7db59fa45f5c57b4ea87e
Alexis Quintero <alexis.quintero@paidy.com>
2026-08-06 08:49:37 +0900

    don't create autocmd if not needed

--- ftplugin/aozora.lua
+++ ftplugin/aozora.lua
@@ -9,15 +9,14 @@ end
 
 vim.bo.makeprg = table.concat(cmd, ' ') .. ' % %<.html'
 
-build.check_tool(tool)
-
-vim.api.nvim_create_autocmd('BufWritePost', {
-  buffer = 0,
-  callback = function()
-    if not build.check_tool(tool) then return end
-    local input = vim.fn.expand('%:p')
-    local output = vim.fn.expand('%:p:r') .. '.html'
-    vim.notify(tool .. ': building...', vim.log.levels.INFO)
-    vim.system(vim.list_extend(vim.list_slice(cmd), { input, output }), { text = true }, build.notify_result(tool))
-  end,
-})
+if build.check_tool(tool) then
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    buffer = 0,
+    callback = function()
+      local input = vim.fn.expand('%:p')
+      local output = vim.fn.expand('%:p:r') .. '.html'
+      vim.notify(tool .. ': building...', vim.log.levels.INFO)
+      vim.system(vim.list_extend(vim.list_slice(cmd), { input, output }), { text = true }, build.notify_result(tool))
+    end,
+  })
+end
--- ftplugin/plantuml.lua
+++ ftplugin/plantuml.lua
@@ -2,14 +2,13 @@ local build = require('build')
 
 vim.bo.makeprg = 'plantuml %'
 
-build.check_tool('plantuml')
-
-vim.api.nvim_create_autocmd('BufWritePost', {
-  buffer = 0,
-  callback = function()
-    if not build.check_tool('plantuml') then return end
-    local cmd = vim.fn.split(vim.fn.expandcmd(vim.bo.makeprg))
-    vim.notify(cmd[1] .. ': building...', vim.log.levels.INFO)
-    vim.system(cmd, { text = true }, build.notify_result(cmd[1]))
-  end,
-})
+if build.check_tool('plantuml') then
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    buffer = 0,
+    callback = function()
+      local cmd = vim.fn.split(vim.fn.expandcmd(vim.bo.makeprg))
+      vim.notify(cmd[1] .. ': building...', vim.log.levels.INFO)
+      vim.system(cmd, { text = true }, build.notify_result(cmd[1]))
+    end,
+  })
+end
